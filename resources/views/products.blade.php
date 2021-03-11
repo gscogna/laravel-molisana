08d0d7a607e2e18d782938db48b5930bd6a214f6
Gennaro <scognamiglioge@hotmail.it>
2021-03-11 19:19:28 +0100

    Modifiche file config e aggiunta del index

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 <div class="container">
     <div class="card-container">
-        @foreach ($paste as $pasta)
+        @foreach ($paste as $key => $pasta)
             <div class="card">
                 <img src=" {{ $pasta ['src'] }} " alt="{{ $pasta ['titolo'] }}">
                 <div class="overlay">
-                    <a href="#"> {{ $pasta['titolo'] }} </a>
+                    <a href=" {{ route('pagina-dettagli', ['id' => $key'] ) }} "> {{ $pasta['titolo'] }} </a>
                 </div>
             </div>
         @endforeach
